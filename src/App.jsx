6e114dd50be244a46c489ed3f620ed762808ac1c
Tyler Bray <tylerhbray@gmail.com>
2024-04-09 17:15:48 -0700

    filtering

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,49 @@
 import { useState } from "react";
+import Note from "./components/Note";
 
-const Button = (props) => (
-  <button onClick={props.handleClick}>{props.text}</button>
-);
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState("");
+  const [showAll, setShowAll] = useState(true);
 
-const App = () => {
-  const [value, setValue] = useState(10);
+  const addNote = (event) => {
+    event.preventDefault();
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
+    };
 
-  const setToValue = (newValue) => {
-    console.log("value now", newValue);
-    setValue(newValue);
+    setNotes(notes.concat(noteObject));
+    setNewNote("");
+  };
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter((note) => note.important === true);
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value);
+    setNewNote(event.target.value);
   };
 
   return (
     <div>
-      {value}
-      <Button handleClick={() => setToValue(1000)} text="thousand" />
-      <Button handleClick={() => setToValue(0)} text="reset" />
-      <Button handleClick={() => setToValue(value + 1)} text="increment" />
+      <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? "important" : "all"}
+        </button>
+      </div>
+      <ul>
+        {notesToShow.map((note) => (
+          <Note key={note.id} note={note} />
+        ))}
+      </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
     </div>
   );
 };
